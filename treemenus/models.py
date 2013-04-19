a3ac60d8bf6aa48a1f021f508ba3398a65b8b9f5
Stanislav Panasik <spanasik@gmail.com>
2013-04-19 19:40:16 +0600

    menu item icon, models.py PEP8

--- treemenus/models.py
+++ treemenus/models.py
@@ -6,13 +6,20 @@ from django.utils.translation import ugettext as _
 
 
 class MenuItem(models.Model):
-    parent = models.ForeignKey('self', verbose_name=ugettext_lazy('parent'), null=True, blank=True)
+    parent = models.ForeignKey('self', verbose_name=ugettext_lazy('parent'),
+                               null=True, blank=True)
     caption = models.CharField(ugettext_lazy('caption'), max_length=50)
     url = models.CharField(ugettext_lazy('URL'), max_length=200, blank=True)
-    named_url = models.CharField(ugettext_lazy('named URL'), max_length=200, blank=True)
-    level = models.IntegerField(ugettext_lazy('level'), default=0, editable=False)
-    rank = models.IntegerField(ugettext_lazy('rank'), default=0, editable=False)
-    menu = models.ForeignKey('Menu', related_name='contained_items', verbose_name=ugettext_lazy('menu'), null=True, blank=True, editable=False)
+    named_url = models.CharField(ugettext_lazy('named URL'),
+                                 max_length=200, blank=True)
+    level = models.IntegerField(ugettext_lazy('level'),
+                                default=0, editable=False)
+    rank = models.IntegerField(ugettext_lazy('rank'),
+                               default=0, editable=False)
+    menu = models.ForeignKey('Menu', related_name='contained_items',
+                             verbose_name=ugettext_lazy('menu'), null=True,
+                             blank=True, editable=False)
+    icon = models.ImageField('icon', upload_to='menu_icons')
 
     def __unicode__(self):
         return self.caption
@@ -31,17 +38,23 @@ class MenuItem(models.Model):
             new_parent = self.parent
             old_parent = MenuItem.objects.get(pk=self.pk).parent
             if old_parent != new_parent:
-                #If so, we need to recalculate the new ranks for the item and its siblings (both old and new ones).
+                # If so, we need to recalculate the new ranks for the item
+                # and its siblings (both old and new ones).
                 if new_parent:
-                    clean_ranks(new_parent.children())  # Clean ranks for new siblings
+                    #  Clean ranks for new siblings
+                    clean_ranks(new_parent.children())
                     self.rank = new_parent.children().count()
-                super(MenuItem, self).save(force_insert, **kwargs)  # Save menu item in DB. It has now officially changed parent.
+                # Save menu item in DB. It has now officially changed parent.
+                super(MenuItem, self).save(force_insert, **kwargs)
                 if old_parent:
-                    clean_ranks(old_parent.children())  # Clean ranks for old siblings
+                    #  Clean ranks for old siblings
+                    clean_ranks(old_parent.children())
             else:
-                super(MenuItem, self).save(force_insert, **kwargs)  # Save menu item in DB
+                # Save menu item in DB
+                super(MenuItem, self).save(force_insert, **kwargs)
 
-        else:  # Saving the menu item for the first time (i.e creating the object)
+        else:
+            # Saving the menu item for the first time (i.e creating the object)
             if not self.has_siblings():
                 # No siblings - initial rank is 0.
                 self.rank = 0
@@ -54,7 +67,8 @@ class MenuItem(models.Model):
         # If level has changed, force children to refresh their own level
         if old_level != self.level:
             for child in self.children():
-                child.save()  # Just saving is enough, it'll refresh its level correctly.
+                # Just saving is enough, it'll refresh its level correctly.
+                child.save()
 
     def delete(self):
         from treemenus.utils import clean_ranks
@@ -81,7 +95,9 @@ class MenuItem(models.Model):
         if not self.parent:
             return MenuItem.objects.none()
         else:
-            if not self.pk:  # If menu item not yet been saved in DB (i.e does not have a pk yet)
+            if not self.pk:
+                # If menu item not yet been saved in DB
+                # (i.e does not have a pk yet)
                 return self.parent.children()
             else:
                 return self.parent.children().exclude(pk=self.pk)
@@ -92,7 +108,8 @@ class MenuItem(models.Model):
     def children(self):
         _children = MenuItem.objects.filter(parent=self).order_by('rank',)
         for child in _children:
-            child.parent = self  # Hack to avoid unnecessary DB queries further down the track.
+            # Hack to avoid unnecessary DB queries further down the track.
+            child.parent = self
         return _children
 
     def has_children(self):
@@ -101,14 +118,18 @@ class MenuItem(models.Model):
 
 class Menu(models.Model):
     name = models.CharField(ugettext_lazy('name'), max_length=50)
-    root_item = models.ForeignKey(MenuItem, related_name='is_root_item_of', verbose_name=ugettext_lazy('root item'), null=True, blank=True, editable=False)
+    root_item = models.ForeignKey(MenuItem, related_name='is_root_item_of',
+                                  verbose_name=ugettext_lazy('root item'),
+                                  null=True, blank=True, editable=False)
 
     def save(self, force_insert=False, **kwargs):
         if not self.root_item:
             root_item = MenuItem()
             root_item.caption = _('root')
-            if not self.pk:  # If creating a new object (i.e does not have a pk yet)
-                super(Menu, self).save(force_insert, **kwargs)  # Save, so that it gets a pk
+            if not self.pk:
+                # If creating a new object (i.e does not have a pk yet)
+                # Save, so that it gets a pk
+                super(Menu, self).save(force_insert, **kwargs)
                 force_insert = False
             root_item.menu = self
             root_item.save()  # Save, so that it gets a pk
